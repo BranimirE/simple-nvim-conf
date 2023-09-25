ff0864536257681e28ebdde839257ab05a55d0ea
Branimir Espinoza <brani.espinoza@gmail.com>
2023-09-25 15:52:00 -0400

    Updating packages

--- lua/myplugins.lua
+++ lua/myplugins.lua
@@ -715,10 +715,10 @@ return {
     dependencies = {'benmills/vimux'}, -- Execute tmux commands from neovim. Used by my custom command 'RunNpmCommand'
     init = function()
       ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.input = function(...)
-        require('lazy').load({ plugins = { 'dressing.nvim' } })
-        return vim.ui.input(...)
-      end
+      -- vim.ui.input = function(...)
+      --   require('lazy').load({ plugins = { 'dressing.nvim' } })
+      --   return vim.ui.input(...)
+      -- end
       ---@diagnostic disable-next-line: duplicate-set-field
       vim.ui.select = function(...)
         require('lazy').load({ plugins = { 'dressing.nvim' } })
