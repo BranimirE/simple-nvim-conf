0030c7e4369686767ab08fade59477129e437c5f
Branimir Espinoza <brani.espinoza@gmail.com>
2023-10-30 18:36:52 -0400

    Enabling arrow keys as they are used to jump quickfix items

--- lua/myplugins.lua
+++ lua/myplugins.lua
@@ -752,6 +752,12 @@ return {
     dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
     opts = {
       disabled_filetypes = { 'qf', 'netrw', 'NvimTree', 'lazy', 'mason', 'Trouble' },
+      disabled_keys = {
+        ['<Up>'] = {},
+        ['<Down>'] = {},
+        ['<Left>'] = {},
+        ['<Right>'] = {},
+      }
     },
     event = { 'BufReadPost', 'BufNewFile' },
   },
