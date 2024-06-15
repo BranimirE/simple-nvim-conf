b87c39e8470e0220df7dbf94a80488d6819515ca
Branimir Espinoza <brani.espinoza@gmail.com>
2024-06-14 21:53:55 -0400

    Fixing lsp configuration for new neovim versions

--- lua/myconfig/lsp.lua
+++ lua/myconfig/lsp.lua
@@ -13,6 +13,7 @@ local diagnostic_icons = {
 local M = {}
 
 --- Returns the editor's capabilities + some overrides.
+--- It is used by the myplugins.lua file
 ---@return lsp.ClientCapabilities
 M.client_capabilities = function()
   return vim.tbl_deep_extend(
@@ -31,7 +32,7 @@ M.client_capabilities = function()
 end
 
 --- Sets up LSP keymaps and autocommands for the given buffer.
----@param client lsp.Client
+---@param client vim.lsp.Client
 ---@param bufnr integer
 local function on_attach(client, bufnr)
   local mymappings = require('mymappings')
@@ -44,9 +45,7 @@ local function on_attach(client, bufnr)
       myutils.load_mapping(mapping)
     end
   end
-  -- myutils.log('client.name='..client.name)
-  -- myutils.log('client.server_capabilities.documentFormattingProvider='..vim.inspect(client.server_capabilities.documentFormattingProvider))
-  -- myutils.log('client.server_capabilities.documentRangeFormattingProvider='..vim.inspect(client.server_capabilities.documentRangeFormattingProvider))
+
   if client.supports_method(methods.textDocument_documentHighlight) then
     local under_cursor_highlights_group =
         vim.api.nvim_create_augroup('branimir/cursor_highlights', { clear = false })
@@ -74,7 +73,7 @@ local function on_attach(client, bufnr)
       local mode = vim.api.nvim_get_mode().mode
       -- TODO:: Use this line in older neovim versions
       -- vim.lsp.inlay_hint.enable(bufnr, mode == 'n' or mode == 'v')
-      vim.lsp.inlay_hint.enable(mode == 'n' or mode == 'v')
+      vim.lsp.inlay_hint.enable(mode == 'n' or mode == 'v', { bufnr = bufnr })
     end, 500)
 
     vim.api.nvim_create_autocmd('InsertEnter', {
@@ -84,7 +83,7 @@ local function on_attach(client, bufnr)
       callback = function()
         -- TODO:: Use this line in older neovim versions
         -- vim.lsp.inlay_hint.enable(bufnr, false)
-        vim.lsp.inlay_hint.enable(false)
+        vim.lsp.inlay_hint.enable(false, { bufnr = bufnr })
       end,
     })
     vim.api.nvim_create_autocmd('InsertLeave', {
@@ -94,7 +93,7 @@ local function on_attach(client, bufnr)
       callback = function()
         -- TODO:: Use this line in older neovim versions
         -- vim.lsp.inlay_hint.enable(bufnr, true)
-        vim.lsp.inlay_hint.enable(true)
+        vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
       end,
     })
   end
@@ -115,6 +114,7 @@ vim.cmd('highlight LspDiagnosticsLineNrHint gui=bold guifg=#862AA1 guibg=#312e3a
 vim.diagnostic.config {
   virtual_text = {
     prefix = '',
+    spacing = 2,
     format = function(diagnostic)
       local icon = diagnostic_icons[vim.diagnostic.severity[diagnostic.severity]]
       local message = vim.split(diagnostic.message, '\n')[1]
@@ -137,6 +137,7 @@ vim.diagnostic.config {
 
 -- Override the virtual text diagnostic handler so that the most severe diagnostic is shown first.
 local show_handler = vim.diagnostic.handlers.virtual_text.show
+assert(show_handler)
 local hide_handler = vim.diagnostic.handlers.virtual_text.hide
 vim.diagnostic.handlers.virtual_text = {
   show = function(ns, bufnr, diagnostics, opts)
@@ -209,7 +210,7 @@ local function enhanced_float_handler(handler, focusable)
 
     -- Add keymaps for opening links.
     if focusable and not vim.b[bufnr].markdown_keys then
-      vim.keymap.set('n', 'K', function()
+      vim.keymap.set('n', '<leader>l', function()
         -- Vim help links.
         local url = (vim.fn.expand '<cWORD>' --[[@as string]]):match '|(%S-)|'
         if url then
@@ -221,7 +222,7 @@ local function enhanced_float_handler(handler, focusable)
         local from, to
         from, to, url = vim.api.nvim_get_current_line():find '%[.-%]%((%S-)%)'
         if from and col >= from and col <= to then
-          vim.system({ 'open', url }, nil, function(res)
+          vim.system({ 'firefox', url }, nil, function(res)
             if res.code ~= 0 then
               vim.notify('Failed to open URL' .. url, vim.log.levels.ERROR)
             end
