e338aecc10205058f15ba6d91ae7206b628cfe16
Branimir Espinoza <brani.espinoza@gmail.com>
2024-07-04 19:14:35 -0400

    Fixing lazydev config

--- lua/myplugins.lua
+++ lua/myplugins.lua
@@ -582,6 +582,14 @@ return {
       {
         'folke/lazydev.nvim',
         ft = 'lua', -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "luvit-meta/library", words = { "vim%.uv" } },
+          },
+        },
+        dependencies = 'Bilal2453/luvit-meta'
       },
     },
     config = function()
