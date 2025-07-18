a569176e691182258725eb7d470483e91735598f
Branimir Espinoza <brani.espinoza@gmail.com>
2025-07-17 21:38:49 -0400

    Introducing flash.nvim

--- lua/myplugins.lua
+++ lua/myplugins.lua
@@ -1298,5 +1298,43 @@ return {
         end
       end
     end,
+  },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    opts = {
+      jump = { nohlsearch = true },
+      prompt = {
+        win_config = {
+          border = 'none',
+          -- Place the prompt above the statusline.
+          row = -3,
+        },
+      },
+      search = {
+        exclude = {
+          'flash_prompt',
+          'qf',
+          function(win)
+            -- Non-focusable windows.
+            return not vim.api.nvim_win_get_config(win).focusable
+          end,
+        },
+      },
+      modes = {
+        -- Enable flash when searching with ? or /
+        search = { enabled = false },
+      },
+    },
+    keys = {
+      {
+        's',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').jump()
+        end,
+        desc = 'Flash',
+      },
+    },
   }
 }
