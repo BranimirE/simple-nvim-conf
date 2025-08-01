3d9d9adeae0aa00c31145e501d421f5a318c57ca
Branimir Espinoza <brani.espinoza@gmail.com>
2025-07-31 20:43:38 -0400

    Fixing fzf live_grep_glob retirement

--- lua/mymappings.lua
+++ lua/mymappings.lua
@@ -154,7 +154,7 @@ M = {
   fzf_lua = function()
     return {
       { '<leader>ff', '<cmd>FzfLua files<cr>',          desc = 'Find files' },
-      { '<leader>fg', '<cmd>FzfLua live_grep_glob<cr>', desc = 'Find Grep' },
+      { '<leader>fg', '<cmd>FzfLua live_grep<cr>', desc = 'Find Grep' },
       { '<leader>fg', '<cmd>FzfLua grep_visual<cr>',    desc = 'Grep Visual', mode = 'x' },
       { '<leader>fp', '<cmd>FzfLua resume<cr>',         desc = 'Grep Resume' },
     }
